8fd0fdd5fe0a352aa05fff5cd47ac8c9e7d9286e
vishal <111978209+vishalbana@users.noreply.github.com>
2024-10-08 14:14:31 +0530

    Update calc2.py

--- calc2.py
+++ calc2.py
@@ -1,4 +1,5 @@
 # Token types 
+
 # EOF (end-of-file) token is used to indicate that
 # there is no more input left for lexical analysis
 INTEGER, PLUS, MINUS, MULTIPLY, DIVISION, EOF = 'INTEGER', 'PLUS', 'MINUS', 'MULTIPLY', 'DIVISION', 'EOF'
